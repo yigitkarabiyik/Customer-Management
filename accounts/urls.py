4daffc291e95410c60e870a571c58ba02a8d55be
yigitkarabiyik <yigitkarabiyik8@gmail.com>
2021-03-02 16:02:25 +0200

    add existing file

---
+++ accounts/urls.py
@@ -0,0 +1,40 @@
+from django.urls import reverse_lazy
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from . import views
+
+app_name='accounts'
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('user/', views.userPage, name='user_page'),
+    path('products/', views.products, name='products'),
+    path('customer/<str:pk_test>/', views.customer, name='customer'),    # pk for customer
+    
+    path('create_order/<str:pk>', views.createOrder, name='create_order'),  # pk for customer
+    path('update_order/<str:pk>/', views.updateOrder, name='update_order'), # pk for order
+    path('delete_order/<str:pk>/', views.deleteOrder, name='delete_order'), # pk for order
+
+    path('register/', views.registerPage, name='register'),
+    path('login/', views.loginPage, name='login'),
+    path('logout/', views.logoutUser, name='logout'),
+
+    path('account/', views.accountSettings, name='account'),
+
+    # Password Reset Email
+    path('reset_password/',
+     auth_views.PasswordResetView.as_view(template_name="accounts/password_reset/password_reset.html"),
+     name="reset_password"),
+
+    path('reset_password_sent/', 
+        auth_views.PasswordResetDoneView.as_view(template_name="accounts/password_reset/password_reset_sent.html"), 
+        name="password_reset_done"),
+
+    path('reset/<uidb64>/<token>/',
+     auth_views.PasswordResetConfirmView.as_view(template_name="accounts/password_reset/password_reset_form.html"), 
+     name="password_reset_confirm"),
+
+    path('reset_password_complete/', 
+        auth_views.PasswordResetCompleteView.as_view(template_name="accounts/password_reset/password_reset_done.html"), 
+        name="password_reset_complete"),
+]
